798ac1ab811d0d09742603e0cfb71c8f5e575d10
Yousuf-Sh <ys937721@gmail.com>
2024-08-21 15:31:21 +0500

    Handle validation error and Add Flash message

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -1,28 +1,47 @@
-@extends('layouts.app')
+@extends('layouts.app') 
 
+@section('styles')
+<style>
+    .error-message{
+        color: red;
+        font-size: 0,8rem;
+    }
+</style>
+@endsection
 @section('title','Add Tasks')
 
-@section('content')
-{{ $errors }}
+@section('content') 
 <form method="POST" action="{{route('tasks.store')}}" >
     @csrf
     <div>
         <label for="title">Title</label>
         <input type="text" name="title" id="text">
+        
+        @error('title')
+        <p class="error-message">{{$message}}</p>            
+        @enderror
     </div>
 
     <div>
         <label for="title">Description</label>
         <textarea name="description" id="description]" rows="5"></textarea>
+        
+        @error('description')
+        <p class="error-message">{{$message}}</p>            
+        @enderror
     </div>
 
     <div>
         <label for="title">Long Description</label>
         <textarea name="long_description" id="long_description]" rows="10"></textarea>
+
+        @error('long_description')
+        <p class="error-message">{{$message}}</p>            
+        @enderror
     </div>
 
     <div>
-        <button type="submit">+ ADD</button>
+        <button type="submit">ADD +</button>
     </div>
 </form>
 
